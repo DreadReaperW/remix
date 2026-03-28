eb4cc8724b9ca4182dd22325cd392c7888dad099
Michael Jackson <mjijackson@gmail.com>
2026-03-27 23:38:02 -0700

    fix: pass remix version through CLI context

--- scripts/generate-remix.ts
+++ scripts/generate-remix.ts
@@ -217,14 +217,7 @@ import { run as runCli } from '${reExportFrom}'
 const remixVersion = ${JSON.stringify(remixVersion)}
 
 export async function run(argv?: string[]): Promise<number> {
-  let previousCliVersion = process.env.REMIX_CLI_VERSION
-  process.env.REMIX_CLI_VERSION = remixVersion
-
-  try {
-    return await runCli(argv)
-  } finally {
-    restoreEnvironmentVariable('REMIX_CLI_VERSION', previousCliVersion)
-  }
+  return await runCli(argv, { remixVersion })
 }
 
 if (import.meta.main) {
@@ -244,13 +237,6 @@ function setExitCode(exitCode: number) {
   Reflect.set(runtimeProcess, 'exitCode', exitCode)
 }
 
-function restoreEnvironmentVariable(name: string, value: string | undefined) {
-  if (value == null) {
-    delete process.env[name]
-  } else {
-    process.env[name] = value
-  }
-}
 `
 }
 
